3c847ebcad822dbc05c2cd632a189a6555e73a3b
Andy Pillay <andypillay93@gmail.com>
2014-12-06 17:00:08 +0000

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -3,43 +3,43 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Jeff hears about a online to-do app and goes to
-		# check the homepage
-		self.browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-		# He notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# He is invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
 
-# He types "Buy shin pads" into a text box (His hobby is football)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
 
-# When he hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do lists
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-# There is still a text box inviting her to add another item. He
-# enters "Buy food for gym"
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-# The page updates again, and now shows both items on his lists
+        # The page updates again, and now shows both items on her list
 
-# Jeff wonders whether the site will remember his list. The he sees
-# that the site has a generated a unique URL for him -- There is some
-# explantory text to that effect.
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
-# He visits that URL - His to do list is still there.
+        # She visits that URL - her to-do list is still there.
 
-# Happy, Jeff goes to sleep
+        # Satisfied, she goes back to sleep
+
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
-
-browser.quit()
+    unittest.main(warnings='ignore')
